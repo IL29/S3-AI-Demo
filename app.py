7e1a17f078463c19efeb7421e9b567a24b9f3a52
IL29 <pb020102@gmail.com>
2026-04-29 02:12:19 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
-import os
 from fastapi import FastAPI, UploadFile
-import boto3
 from rag import add_document, search
 
-app = FastAPI()
+import ollama  
 
-s3 = boto3.client("s3")
-BUCKET = "project-genai-bucket"
+app = FastAPI()
 
 
 @app.post("/upload")
@@ -14,43 +11,44 @@ async def upload(file: UploadFile):
     content = await file.read()
     text = content.decode()
 
-    s3.put_object(
-        Bucket=BUCKET,
-        Key=file.filename,
-        Body=content
-    )
-
     add_document(file.filename, text)
 
     return {"message": "uploaded + indexed"}
 
 
 @app.get("/ask")
-def ask(filename: str, question: str):
+def ask(question: str):
 
+  
     context_chunks = search(question, k=3)
     context = "\n".join(context_chunks)
 
+  
     prompt = f"""
-    Answer using the contexts provided below
+Answer by using the rules set below.
 
-    Context:
-    {context}
+Context:
+{context}
 
-    Question:
-    {question}
-    """
+Question:
+{question}
 
-    answer_text = f"(Demo mode)\n\n{context}"
+If the answer is not within the rules say "I'm sorry, I don't understand".
+"""
+
+    try:
+        response = ollama.chat(
+            model="llama3",
+            messages=[{"role": "user", "content": prompt}]
+        )
+
+        answer_text = response["message"]["content"]
+
+    except:
+
+        answer_text = context
 
     return {
         "answer": answer_text,
         "sources": context_chunks
     }
-
-if __name__ == "__main__":
-    import uvicorn
-    import os
-
-    port = int(os.environ.get("PORT", 10000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
